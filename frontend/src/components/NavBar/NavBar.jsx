71d23196b80511e01dab8650ee5b03af16d9d8ed
NeiryAeris <Kukorysenpai3506@gmail.com>
2024-09-29 03:03:09 +0700

    random

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./NavBar.css";
+import icon from "../Assets/cat_icon.jpg";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const handleSearchKeyPress = (event) => {
+    if (event.key === "Enter") {
+      // Perform search action here
+      console.log("Search value:", event.target.value);
+    }
+  };
+  return (
+    <div className="navbar">
+      <div className="icon">
+        <img src={icon} alt="" />
+      </div>
+      <div className="search">
+        <input type="text" onKeyPress={handleSearchKeyPress} placeholder="Type something" />
+      </div>
+      <ul className="nav-menu">
+        <li>
+          <Link style={{ textDecoration: "none", color: "#96969a", display: "flex", gap: "5px" }}>NavTo</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none", color: "#96969a", display: "flex", gap: "5px" }}>NavTo</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none", color: "#96969a", display: "flex", gap: "5px" }}>NavTo</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none", color: "#96969a", display: "flex", gap: "5px" }}>NavTo</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
